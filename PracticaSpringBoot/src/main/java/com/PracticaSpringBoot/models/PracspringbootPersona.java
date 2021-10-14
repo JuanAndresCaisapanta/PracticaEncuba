fac473fd987b3985fc2a8dac2f743191e1de7361
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2021-10-14 03:27:36 -0500

    Cambio tabla base de datos

--- PracticaSpringBoot/src/main/java/com/PracticaSpringBoot/models/PracspringbootPersona.java
+++ PracticaSpringBoot/src/main/java/com/PracticaSpringBoot/models/PracspringbootPersona.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "datper_persona")
+@Table(name = "Persona")
 public class PracspringbootPersona implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
